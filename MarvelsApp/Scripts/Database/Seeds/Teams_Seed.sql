a6371c00853e7c7ddb0ed95f6470ec437c532984
itsjusthajra <hajrafatima20431@gmail.com>
2025-08-11 22:43:24 +0530

    Added Database Seeds

---
+++ MarvelsApp/Scripts/Database/Seeds/Teams_Seed.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO Teams (Name, Description)
+VALUES
+('Avengers', 'Earths mightiest heroes united to protect the world'),
+('X-Men', 'Mutant superheroes protecting a world that fears them'),
+('Guardians of the Galaxy', 'Intergalactic misfits saving the universe'),
+('Fantastic Four', 'Marvel’s first superhero family'),
+('Defenders', 'Street-level heroes teaming up to defend New York'),
+('Inhumans', 'Royal family with unique powers from Terrigen exposure'),
+('Thunderbolts', 'Reformed villains working for redemption or personal gain'),
+('Midnight Sons', 'Supernatural heroes battling dark forces');
